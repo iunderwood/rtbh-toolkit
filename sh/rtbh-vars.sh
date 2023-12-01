6738b2536caef619b20a8942742fc890c8f027b9
Ian A. Underwood <>
2023-12-01 13:00:20 -0500

    Added sample shell scripts for accessing the program components, along with a short script to set RTBH variables.

---
+++ sh/rtbh-vars.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Customize the directories to point to where the VENV is and where the config directory is stored.
+#
+# These contents can be added for all uers in the system, optionally.
+
+export RTBH_VENV=/usr/local/bch/python/venv/rtbh
+export IUPY_CFG=/usr/local/bch/etc
