b4759878ee03f239fc6b90a1fe871beed4eb9829
Ian A. Underwood <ian@underwood-hq.org>
2024-03-06 07:15:59 -0500

    Another iteration...

--- rtbh-toolkit/rtbh-routerunner-xe.py
+++ rtbh-toolkit/rtbh-routerunner-xe.py
@@ -211,20 +211,19 @@ def restconf_fib_size(router, instance_name):
 
     response = router.get('data/Cisco-IOS-XE-fib-oper:fib-oper-data/fib-ni-entry/?fields=num-pfx;num-pfx-fwd;instance-name')
 
+    if response is None:
+        log.debug("Response is None.")
+        return None
+
     # Process a good response.
-    try:
-        if 'status_code' in response:
-            if response.status_code == 200:
-                fib_dict = json.loads(response.content)
-            else:
-                log.debug("Response code: {}".format(response.status_code))
-                return None
+    if 'status_code' in response:
+        if response.status_code == 200:
+            fib_dict = json.loads(response.content)
         else:
-            log.debug("Request failed.  No response code returned.")
+            log.debug("Response code: {}".format(response.status_code))
             return None
-    except TypeError as e:
-        log.error("Problem with response: {}".format(e))
-        pprint.pprint(response)
+    else:
+        log.debug("Request failed.  No response code returned.")
         return None
 
     for instance in fib_dict['Cisco-IOS-XE-fib-oper:fib-ni-entry']:
